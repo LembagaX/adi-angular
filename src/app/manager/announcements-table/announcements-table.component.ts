a6efbacb4ef2927395733ab47557818bce950793
Agung Gunawan <lembagax@gmail.com>
2019-02-25 22:33:18 -0800

    Add some ux

--- src/app/manager/announcements-table/announcements-table.component.ts
+++ src/app/manager/announcements-table/announcements-table.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { AnnouncementService } from 'src/app/announcement.service';
-import { MatTableDataSource } from '@angular/material';
+import { MatTableDataSource, MatDialog } from '@angular/material';
 import { Announcement } from 'src/app/response/announcement';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { LoadingPopupComponent } from 'src/app/partials/loading-popup/loading-popup.component';
 
 @Component({
   selector: 'app-announcements-table',
@@ -18,7 +19,8 @@ export class AnnouncementsTableComponent implements OnInit {
   public colors: { id: number; class: string; color: string}[];
 
   constructor(
-    private _announce: AnnouncementService
+    private _announce: AnnouncementService,
+    private _load: MatDialog
   ) { }
 
   ngOnInit() {
@@ -28,27 +30,48 @@ export class AnnouncementsTableComponent implements OnInit {
       {id: 1, class: 'text-success', color: 'success'},
       {id: 2, class: 'text-warning', color: 'warning'},
       {id: 3, class: 'text-danger', color: 'danger'}
-    ]
-    this._announce.index().subscribe(response => this.announcements = new MatTableDataSource(response));
+    ];
+    this.fetch();
+    this.buildForm();
+  }
+
+  private buildForm() {
     this.form = new FormGroup({
-      message: new FormControl(null, [Validators.required, Validators.minLength(6), Validators.maxLength(255)]),
+      message: new FormControl('', [Validators.required, Validators.minLength(6), Validators.maxLength(255)]),
       color: new FormControl('', [Validators.required]),
-      due: new FormControl(7, [Validators.required, Validators.min(1)])
+      due: new FormControl(null, [Validators.required, Validators.min(1)])
     });
+  }
+
+  private fetch() {
+    this.announcements = null;
+    this._announce.index().subscribe(response => this.announcements = new MatTableDataSource(response));
   }
 
   public submit() {
     if (this.onEdit) {
-      this._announce.update(this.current, this.form.value).subscribe(response => {
+      const dialog = this._load.open(LoadingPopupComponent, { data: 'Updating, please wait'});
+      this._announce.update(this.current, this.form.value).subscribe(() => {
+        this.fetch();
+        this.buildForm();
+        dialog.close();
       });
     } else {
-      this._announce.create(this.form.value).subscribe(response => {
+      const dialog = this._load.open(LoadingPopupComponent, { data: 'Creating, please wait' });
+      this._announce.create(this.form.value).subscribe(() => {
+        this.fetch();
+        this.buildForm();
+        dialog.close();
       });
     }
   }
 
   public destroy(announcement: Announcement) {
-    this._announce.destroy(announcement).subscribe();
+    const dialog = this._load.open(LoadingPopupComponent, { data: 'Destroying, please wait' });
+    this._announce.destroy(announcement).subscribe(() => {
+      this.fetch();
+      dialog.close();
+    });
   }
 
   public edit(announcement: Announcement) {
@@ -56,6 +79,12 @@ export class AnnouncementsTableComponent implements OnInit {
     this.current = announcement;
     this.form.get('message').setValue(announcement.message);
     this.form.get('color').setValue(announcement.color);
+  }
+
+  public reset() {
+    this.buildForm();
+    this.current = null;
+    this.onEdit = false;
   }
 
 }
